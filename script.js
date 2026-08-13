661e9d80b65b7281cc478137499203f4826b6175
cecilia67auruda67 <cecilia.kozlowski@escola.pr.gov.br>
2026-08-13 12:16:15 -0300

    ok

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const likeBtn = document.querySelector("left-action-btn:first-child");
 }
 
 
+//localiza o contador
 
-
-
+  let texNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
 
 
 
